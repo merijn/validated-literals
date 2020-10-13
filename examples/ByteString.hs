032f7b583af2750d70ee47fe415706f54ce0a87f
Merijn Verstraaten <merijn@inconsistent.nl>
2020-10-13 13:41:08 +0200

    Make use of template-haskell 2.16's new typedLift.

--- examples/ByteString.hs
+++ examples/ByteString.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TemplateHaskell #-}
@@ -18,5 +19,11 @@ instance Validate String ByteString where
       where
         nonAsciiVals = filter (not . (<=255) . ord) s
 
+    -- As of template-haskell-2.16.0.0 we no longer need to rely on the hack of
+    -- unsafeTExpCoerce!
+#if MIN_VERSION_template_haskell(2,16,0)
+    liftResult _ v = [|| BS.pack $$(liftTyped (BS.unpack v)) ||]
+#else
     liftResult _ v = unsafeTExpCoerce $
         AppE <$> [| BS.pack |] <*> lift (BS.unpack v)
+#endif
